81f69d52048f907120be9c9dc25e08eb306c37cf
Ye Min Aung <116590221+Ye-Min-Ag@users.noreply.github.com>
2023-09-03 17:31:52 +0630

    Update HCPredict.py

--- HCPredict.py
+++ HCPredict.py
@@ -36,7 +36,7 @@ if uploaded_file is not None:
     image_paths = ['Unknown-2.png','Unknown-3.png','Unknown-4.png']
     for i in image_paths:
         st.image(i, width=500, caption='Scatter Plots')
-    st.write('These Heatmap and Scatter Plots reveal our dataset comes with certain data quality challenges. These may include outliers, inconsistencies, noises, etc.')
+    st.write('These Heatmap and Scatter Plots reveal our dataset comes with certain data quality challenges. These may include outliers, inconsistencies, noises, etc,.')
         
 
 
